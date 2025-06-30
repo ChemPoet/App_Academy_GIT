f05f666266afdbd6d7ecbcf634a1caddfda890e8
Dr. Wynand J. Louw <dr.wynandj.louw@gmail.com>
2025-06-30 09:48:39 +0200

    python
    Created work environment.

---
+++ python/strings.py
@@ -0,0 +1,18 @@
+#Strings
+
+message = 'Hello World!'
+#   Create string in between [' '] or [" "].
+
+print(message)
+
+message1 = "This is Bob's world."
+#   If the string contains apostrophies ('), define string with [" "].
+
+print(message1)
+
+message2 = """Bob's world
+is cool"""
+#   Split a string across multiple lines (i.e., include "enter" as a spacer) by defining the string with [""" """].
+
+print(message2)
+
