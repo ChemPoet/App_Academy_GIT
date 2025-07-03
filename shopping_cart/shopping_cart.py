19c55c2a6b69e4b13610b1f2a794f40cdd8d7385
Dr. Wynand J. Louw <dr.wynandj.louw@gmail.com>
2025-07-03 17:18:02 +0200

    SC
    delete index and style
    started on rough draft for SC

---
+++ shopping_cart/shopping_cart.py
@@ -0,0 +1,33 @@
+#   Shopping Cart
+
+#   1) User input of data – food item, price
+#   2) Loop User input until user wants to Check Out
+#   3) Output all items in User input
+#   4) Calculate total for all items in User input
+
+food_list = []
+price_list = []
+shopping_cart = {}
+
+add_item = str(input("""What food item would you like to add to the list today?
+                     
+    """))
+print()
+
+food_list.append(add_item)
+    
+print(food_list)
+
+item_price = float(input("""How much does the itme cost?
+    R ___.__                     
+      """))
+print()
+
+price_list.append(item_price)
+    
+print(price_list)
+print()
+
+shopping_cart[add_item] = item_price
+
+print(shopping_cart)
